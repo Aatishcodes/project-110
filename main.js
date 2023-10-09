cde10fd1370a9c61422fe2f1b37f209fd0e3a0a0
Aatishcodes <118838033+Aatishcodes@users.noreply.github.com>
2023-10-09 18:30:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,102 @@
+// https://teachablemachine.withgoogle.com/models/V3WMEN2A_/
+
+
+
+Webcam.set({
+    width: 340,
+    height: 300,
+    image_format: "jpeg",
+    jpeg_quality: 90
+});
+
+camera = document.getElementById("camera")
+Webcam.attach(camera)
+
+
+
+function take_snapshot() {
+    Webcam.snap(function (data_uri) {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="' + data_uri + '"/>';
+    });
+
+}
+
+
+prediction_1 = ""
+prediction_2 = ""
+
+
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/EXE72iOrl/model.json", modelLoaded)
+
+
+function modelLoaded() {
+    console.log("model has been loaded")
+
+
+}
+
+function speak() {
+    var synth = window.speechSynthesis
+    speakdata_1 = "The first prediction is - " + prediction_1
+    speakdata_2 = "The second prediction is - " + prediction_2
+    var utterthis = new SpeechSynthesisUtterance(speakdata_1 + speakdata_2)
+    synth.speak(utterthis)
+}
+
+
+function identify_emotion() {
+    img = document.getElementById("captured_image")
+
+    classifier.classify(img, got_result)
+
+}
+
+function got_result(error, result) {
+    if (error) {
+        console.log(error)
+    }
+    else {
+        console.log(result)
+        document.getElementById("result_emotion_name").innerHTML = result[0].label
+        document.getElementById("result_emotion_name2").innerHTML = result[1].label
+        prediction_1 = result[0].label
+        prediction_2 = result[1].label
+        speak()
+
+        if(prediction_1 == "Thumbs Up") {
+            document.getElementById("emoji_image_1").innerHTML = "👍"
+        }
+
+        if(prediction_1 == "ok/ good") {
+            document.getElementById("emoji_image_1").innerHTML = "👌"
+        }
+
+        if(prediction_1 == "Peace Sign") {
+            document.getElementById("emoji_image_1").innerHTML = "✌️"
+        }
+
+        if(prediction_1 == "Thumbs Down") {
+            document.getElementById("emoji_image_1").innerHTML = "👎"
+        }
+
+        if(prediction_1 == "Thumbs Up") {
+            document.getElementById("emoji_image_2").innerHTML = "👍"
+        }
+
+        if(prediction_1 == "ok/ good") {
+            document.getElementById("emoji_image_2").innerHTML = "👌"
+        }
+
+        if(prediction_1 == "Peace Sign") {
+            document.getElementById("emoji_image_2").innerHTML = "✌️"
+        }
+
+        if(prediction_1 == "Thumbs Down") {
+            document.getElementById("emoji_image_2").innerHTML = "👎"
+        }
+
+
+    }
+
+
+}
